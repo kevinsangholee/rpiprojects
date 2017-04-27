1eb526757d2e119ad01cf7514d83dfda08c07ec2
kevinsangholee <kevinsangholee@gmail.com>
2017-04-27 12:59:31 -0700

    Add files via upload

--- ledsimon.py
+++ ledsimon.py
@@ -3,6 +3,7 @@
 
 import RPi.GPIO as GPIO
 import time
+from random import randint
 
 green = 4
 red = 17
@@ -19,9 +20,37 @@ GPIO.setup(gButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(rButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(yButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+level = 1
+sequence = []
+
+def create_sequence(num):
+    sequence = []
+    for i in range(0,num):
+        sequence.append(randint(0,2))
+
+def getPin(n):
+    if n == 0:
+        return green
+    elif n == 1:
+        return red
+    else:
+        return yellow
+
+def play_sequence(lst):
+    for n in lst:
+        GPIO.output(getPin(n), 1)
+        time.sleep(.2)
+        GPIO.output(getPin(n), 0)
+        time.sleep(.2)
+
 try:
     while True:
-        if(not GPIO.input(gButton)):
+        print "Playing sequence..."
+        time.sleep(2)
+        create_sequence(3)
+        play_sequence(sequence)
+
+        """if(not GPIO.input(gButton)):
             print 'Green pressed'
             GPIO.output(green, 1)
             time.sleep(1)
@@ -35,7 +64,7 @@ try:
             print 'Yellow pressed'
             GPIO.output(yellow,1)
             time.sleep(1)
-            GPIO.output(yellow,1)
+            GPIO.output(yellow,0)"""
 except KeyboardInterrupt:
     GPIO.output(green, 0)
     GPIO.output(red, 0)
