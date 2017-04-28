319c61757f5a552aaf02198611ff03f923f8e72b
kevinsangholee <kevinsangholee@gmail.com>
2017-04-28 15:16:57 -0700

    Add files via upload

--- rgbled.py
+++ rgbled.py
@@ -19,23 +19,20 @@ GREEN.start(0)
 BLUE = GPIO.PWM(blue, Freq)
 BLUE.start(0)
 
-def color(R, G, B, on_time):
-    RED.ChangeDutyCycle(R)
-    GREEN.ChangeDutyCycle(G)
-    BLUE.ChangeDutyCycle(B)
-    time.sleep(on_time)
-
-    RED.ChangeDutyCycle(0)
-    GREEN.ChangeDutyCycle(0)
-    BLUE.ChangeDutyCycle(0)
-
 try:
     while True:
-        for x in range(0,2):
-            for y in range(0,2):
-                for z in range(0,2):
-                    for i in range(0,101):
-                        color((x*i),(y*i),(z*i), .02)
+        print "red"
+        for r in range(0,101):
+            RED.ChangeDutyCycle(r)
+            time.sleep(.02)
+        print "green"
+        for g in range(0,101):
+            GREEN.ChangeDutyCycle(g)
+            time.sleep(.02)
+        print "blue"
+        for b in range(0,101):
+            BLUE.ChangeDutyCycle(b)
+            time.sleep(.02)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
